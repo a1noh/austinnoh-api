e9c0dcb142fc2745b56ca3c5b39c4bff55bd75c2
austin noh <noh1austin@gmail.com>
2021-03-03 00:47:46 +0900

    populate db with fake data

--- db/models/portfolio.js
+++ db/models/portfolio.js
@@ -2,8 +2,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const portfolioSchema = new Schema({
-  title: String,
-  description: String,
+  title: { type: String, required: true, maxLength: 128 },
+  project: { type: String, required: true, maxlength: 64 },
+  projectWebsite: { type: String, required: true, maxlength: 128 },
+  location: { type: String, required: true },
+  role: { type: String, required: true },
+  description: { type: String, required: true },
+  startDate: { type: Date, required: true },
+  endDate: { type: Date },
+  createdAt: { type: Date },
 });
 
 module.exports = mongoose.model("Portfolio", portfolioSchema);
---
+++ populate/data.js
@@ -0,0 +1,36 @@
+const data = {
+  portfolios: [
+    {
+      title: "Job in Netcentric",
+      project: "Netcentric",
+      projectWebsite: "www.google.com",
+      location: "Spain, Barcelona",
+      role: "Engineer",
+      description: "Doing something, programing....",
+      startDate: "01/01/2014",
+      endDate: "01/01/2016",
+    },
+    {
+      title: "Job in Siemens",
+      project: "Siemens",
+      projectWebsite: "www.google.com",
+      location: "Slovakia, Kosice",
+      role: "Software Engineer",
+      description: "Responsoble for parsing framework for JSON medical data.",
+      startDate: "01/01/2011",
+      endDate: "01/01/2013",
+    },
+    {
+      title: "Work in USA",
+      project: "WhoKnows",
+      projectWebsite: "www.google.com",
+      location: "USA, Montana",
+      role: "Housekeeping",
+      description: "So much responsibility....Overloaaaaaad",
+      startDate: "01/01/2010",
+      endDate: "01/01/2011",
+    },
+  ],
+};
+
+module.exports = data;
