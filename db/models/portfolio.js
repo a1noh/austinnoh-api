2f2633c923d57f8e6bec5e1058160578cbc1da75
austin noh <noh1austin@gmail.com>
2021-03-12 13:05:07 +0900

    project->portfolio

--- db/models/portfolio.js
+++ db/models/portfolio.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema;
 
 const portfolioSchema = new Schema({
   title: { type: String, required: true, maxLength: 128 },
-  project: { type: String, required: true, maxlength: 64 },
-  projectWebsite: { type: String, required: true, maxlength: 128 },
+  portfolio: { type: String, required: true, maxlength: 64 },
+  portfolioWebsite: { type: String, required: true, maxlength: 128 },
   location: { type: String, required: true },
   role: { type: String, required: true },
   description: { type: String, required: true },
