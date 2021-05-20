1b64172dbc8b29ed94491fd81e40391080ef226d
austin noh <noh1austin@gmail.com>
2021-05-20 10:26:50 -0400

    prod dev configs

---
+++ config/prod.js
@@ -0,0 +1,11 @@
+module.exports = {
+  DB_URI: process.env.DB_URI,
+  AUTH0_NAMESPACE: process.env.AUTH0_NAMESPACE,
+  AUTH0_TOKEN_URL: process.env.AUTH0_TOKEN_URL,
+  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+  AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
+  AUTH0_AUDIENCE: process.env.AUTH0_AUDIENCE,
+  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+};
+
+//mongodb+srv://austin:austin1@cluster0.8l7ej.mongodb.net/portfolio?retryWrites=true&w=majority
